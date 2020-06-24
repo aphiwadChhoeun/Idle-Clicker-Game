27297a4871e1ad54f02b9ffe4d870e18c89c4f7b
Aphiwad <aphiwad.chhoeun@cbsinteractive.com>
2020-06-24 14:26:06 -0700

    update assets

--- src/components/ActionPanel.vue
+++ src/components/ActionPanel.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="action__container">
-    <action-button />
     <div class="action__wrapper">
       <coin-pocket />
     </div>
+    <action-button />
   </div>
 </template>
 
@@ -19,7 +19,7 @@
   box-shadow: 0 -15px 5px rgba(0, 0, 0, 0.1);
 
   .action__wrapper {
-    position: relative;
+    position: absolute;
     width: 100%;
     height: 100%;
     display: flex;
