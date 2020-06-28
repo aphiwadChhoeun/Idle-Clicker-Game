249e431bb09755b05ff5a884c1346451285ef0a2
Aphiwad <aphiwad.chhoeun@cbsinteractive.com>
2020-06-27 17:52:15 -0700

    add debug button for reset game

--- src/components/modals/SettingsModal.vue
+++ src/components/modals/SettingsModal.vue
@@ -10,6 +10,15 @@
           max="100"
           v-on:input="onMusicChanged"
         />
+
+        <div class="debug__container">
+          <div>Debug only</div>
+          <div class="debug__clear__save">
+            <button class="button is-primary" @click="resetGame">
+              !!Reset game!!
+            </button>
+          </div>
+        </div>
       </div>
     </template>
   </base-modal>
@@ -35,6 +44,23 @@ export default {
   },
 
   methods: {
+    // DELETE LATER
+    resetGame() {
+      this.$swal({
+        icon: 'warning',
+        title: 'This will reset your game!!!',
+        showCancelButton: true,
+        confirmButtonColor: '#e7305b',
+        cancelButtonColor: '#0fabbc',
+        confirmButtonText: 'Yes, reset it!'
+      }).then(result => {
+        if (result.value) {
+          localStorage.removeItem('ClickerGame')
+          window.location.reload()
+        }
+      })
+    },
+
     onMusicChanged(val) {
       this.setMusicVolume(val / 100)
     },
@@ -50,5 +76,10 @@ export default {
   padding: 0 4rem;
   display: flex;
   flex-direction: column;
+
+  // DELETE LATER
+  .debug__container {
+    margin: 2rem;
+  }
 }
 </style>
